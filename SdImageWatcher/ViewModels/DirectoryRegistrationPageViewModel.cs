bd0e97413765952bae5052eb08a1e2816c2a4cf1
Akn <sibata486@gmail.com>
2024-01-05 22:05:41 +0900

    add / MainWindow から ディレクトリ登録ページへ移動する処理を実装

--- SdImageWatcher/ViewModels/DirectoryRegistrationPageViewModel.cs
+++ SdImageWatcher/ViewModels/DirectoryRegistrationPageViewModel.cs
@@ -2,15 +2,21 @@ using System;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
+using SdImageWatcher.Models;
 
 namespace SdImageWatcher.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class DirectoryRegistrationPageViewModel : BindableBase, IDialogAware
     {
+        private DatabaseContext databaseContext;
+        private string directoryPath = string.Empty;
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
+
+        public string DirectoryPath { get => directoryPath; set => SetProperty(ref directoryPath, value); }
 
         public DelegateCommand CloseCommand => new (() =>
         {
@@ -28,6 +34,7 @@ namespace SdImageWatcher.ViewModels
 
         public void OnDialogOpened(IDialogParameters parameters)
         {
+            databaseContext = parameters.GetValue<DatabaseContext>(nameof(DatabaseContext));
         }
     }
 }
