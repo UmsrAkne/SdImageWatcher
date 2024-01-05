b6d0d1099ce068d3d497fc0f96dd619fc82e4a60
Akn <sibata486@gmail.com>
2024-01-05 21:45:02 +0900

    add / 登録画面のビューとビューモデルを追加

--- SdImageWatcher/ViewModels/MainWindowViewModel.cs
+++ SdImageWatcher/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Mvvm;
+using Prism.Services.Dialogs;
 using SdImageWatcher.Models;
 
 namespace SdImageWatcher.ViewModels
@@ -6,10 +7,12 @@ namespace SdImageWatcher.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class MainWindowViewModel : BindableBase
     {
+        private readonly IDialogService dialogService;
         private string title = "Prism Application";
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IDialogService dialogService)
         {
+            this.dialogService = dialogService;
             using var db = new DatabaseContext();
             db.Database.EnsureCreated();
         }
